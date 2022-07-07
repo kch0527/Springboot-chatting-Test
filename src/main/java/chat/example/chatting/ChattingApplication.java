582e24f2141fbca0e4656cf1c917c0fe331ece88
chan <jhgu128@naver.com>
2022-07-07 12:06:01 +0900

    2022.07.07 서비스 고민

--- src/main/java/chat/example/chatting/ChattingApplication.java
+++ src/main/java/chat/example/chatting/ChattingApplication.java
@@ -2,13 +2,10 @@ package chat.example.chatting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ChattingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ChattingApplication.class, args);
     }
-
 }
