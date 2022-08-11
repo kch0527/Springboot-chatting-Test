6976e8ac0094220ba5cf3b6090957cb9f73605ae
chan <jhgu128@naver.com>
2022-08-11 19:06:28 +0900

    2022.08.11 채팅방 구분 진행중

---
+++ src/main/java/chat/example/chatting/ChatController.java
@@ -0,0 +1,9 @@
+package chat.example.chatting;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+
+public class ChatController {
+
+}
