e871d38955376784aaba3d34789262c9878d485a
chan <jhgu128@naver.com>
2022-08-13 21:46:27 +0900

    2022.08.13 STOMP를 사용한 채팅

--- src/main/java/chat/example/ChattingApplication.java
+++ src/main/java/chat/example/ChattingApplication.java
@@ -1,4 +1,4 @@
-package chat.example.chatting;
+package chat.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/chat/example/chatting/ChatController.java
+++
@@ -1,9 +0,0 @@
-package chat.example.chatting;
-
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-
-public class ChatController {
-
-}
